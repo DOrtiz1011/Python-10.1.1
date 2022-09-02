c2510032db16057ae1722d94b43bc96b47df926d
Daniel Ortiz <DOrtiz1011@gmail.com>
2022-09-01 21:35:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,41 @@
 
 # Question 1: Create two variables. One should be a string data type, and the other should be of type int.
 
+stringVar = "Text"
+intVar = 5
+
+
 # Use a single print statement to print both variables:
 
+print(stringVar, intVar)
 
 # Question 2: Create a Python function that prints a greeting with a name as the parameter.
 
+def greeting(message):
+    print(message)
+
 # Invoke the function with a name argument of your choosing:
 
-
+greeting("Hello World")
 
 ## Section 2 - lists:
 
 # Question 3: Create a list of your favorite movies with  at least 4 elements:
 
+list = ["1", "2", "3"]
+
 
 # Question 4: Use a print statement to print the list item at the index of 1:
 
+print(list)
 
 # Question 5: Append a movie to the end of your list:
 
+list.append("4")
+
 
 # Use a print statement to print this ammended list:
-
+print(list)
 
 
 ## Section 3 - dictionaries:
@@ -32,35 +45,51 @@
 #The keys should be: "color" and "number".
 #Fill out the values on your own:
 
+cellphone = {
+    "color" : "red",
+    "number" : "5555555555"
+}
+
 
 # Question 7: Access a value from inside the dictionary (Try to print the value of the 'color' property).
 
-
+print(cellphone.get("color"))
 
 
 ## Section 4 - strings:
 
 # Question 8: Create a variable and store a string with multiple words in it:
 
+multiString = "How now brown cow"
+
 # Question 9: Utilize the method that capitalizes the first letter of each word in your string - store this new string in a new variable:
 
 # Use a print statement to print the new string:
 
-
+print(multiString.title())
 
 
 #Bonus:
 
 # Question 10: Uncomment and look into this nested dictionary - try to relate your knowledge of objects and arrays in JavaScript.
 
-# students_in_order = {
-#   1: {'name': 'Esteban', 'age': '27', 'eye color': 'blue'},
-#   2: {'name': 'Jackson', 'age': '22', 'eye color': 'brown'},
-#   3: {'name': 'Zayn', 'age': '26', 'eye color': 'green'}
-# }
+students_in_order = {
+  1: {'name': 'Esteban', 'age': '27', 'eye color': 'blue'},
+  2: {'name': 'Jackson', 'age': '22', 'eye color': 'brown'},
+  3: {'name': 'Zayn', 'age': '26', 'eye color': 'green'}
+}
 
 # Question 10 A: Write a print function that outputs the second student in the dictionary
 
+x = students_in_order.get(2).get("name")
+print (x)
+
 # Question 10 B: Write a print statement that outputs the name "Zayn" using the dictionary
 
+x = students_in_order.get(3).get("name")
+print (x)
+
 # Question 10 C: Write a print statment that outputs the age of Esteban from the dictionary
+
+x = students_in_order.get(2).get("age")
+print (x)
